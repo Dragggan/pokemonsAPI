2427cbdcf7515dc1840613ec61ddeecaf0340970
Dragan, Djuza <ext.dragan.djuza@centogene.com>
2020-11-17 22:32:14 +0100

    need to add react router

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,58 +1,15 @@
 import React from 'react';
-
+import {Table} from 'semantic-ui-react'
 function Main({ allPokemonData }) {
 
-
-    const tableRowEvents = {
-        onClick: (e, row, rowIndex) => {
-          console.log(`clicked on row with index: ${rowIndex}`);
-        },
-        onMouseEnter: (e, row, rowIndex) => {
-          console.log(`enter on row with index: ${rowIndex}`);
-        }
-     }
+   const  renderBodyRow =(name,url)=> {
+        console.log(name,url);
+    }
 
 
     return (
         <div>
-
-<table class="ui celled table">
-  <thead>
-    <tr><th>Name</th>
-    <th>Age</th>
-    <th>Job</th>
-  </tr></thead>
-  <tbody>
-    <tr>
-      <td data-label="Name">James</td>
-      <td data-label="Age">24</td>
-      <td data-label="Job">Engineer</td>
-    </tr>
-    <tr>
-      <td data-label="Name">Jill</td>
-      <td data-label="Age">26</td>
-      <td data-label="Job">Engineer</td>
-    </tr>
-    <tr>
-      <td data-label="Name">Elyse</td>
-      <td data-label="Age">24</td>
-      <td data-label="Job">Designer</td>
-    </tr>
-  </tbody>
-</table>
-
-
-
-
-
-
-
-
-
-
-
-
-            {/* <Table striped bordered hover  rowEvents={ tableRowEvents }>
+            <table className="ui selectable celled table ">
                 <thead>
                     <tr>
                         <th>Name</th>
@@ -60,17 +17,16 @@ function Main({ allPokemonData }) {
                     </tr>
                 </thead>
                 <tbody>
-                    {allPokemonData && allPokemonData.map((m,i) => {
+                    {allPokemonData && allPokemonData.map((m, i) => {
                         return (
-                            <tr key={i}>
+                            <tr key={i} onClick={() => {renderBodyRow(m.name,m.url)}}>
                                 <td>{m.name}</td>
                                 <td><span className="url">{m.url}</span></td>
                             </tr>
                         );
                     })}
-
                 </tbody>
-            </Table> */}
+            </table>
         </div>
     );
 }
