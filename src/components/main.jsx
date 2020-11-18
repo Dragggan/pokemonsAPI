3bb43c004466f9177fcf9ff509a2541131c6cda7
Dragan, Djuza <ext.dragan.djuza@centogene.com>
2020-11-18 23:15:08 +0100

    started with new liberie

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import _ from 'lodash'
-import { Grid, Image } from 'semantic-ui-react'
+import _ from 'lodash';
 
-    // const columns = _.times(16, (i) => (
-    //     <Grid.Column key={i}>
-    //         <h5>sfdsfds</h5>
-    //       <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
-    //     </Grid.Column>
-    //   ))
-      
-      const Main = ({allPokemonData}) => {
+import { MDBTable, MDBTableBody, MDBTableHead } from 'mdbreact';
 
-        return (
-            <div>sdfdsfsd</div>
-        )
+// const columns = _.times(16, (i) => (
+//     <Grid.Column key={i}>
+//         <h5>sfdsfds</h5>
+//       <Image src='https://react.semantic-ui.com/images/wireframe/image.png' />
+//     </Grid.Column>
+//   ))
 
-        // <Grid>{columns}</Grid>
-      }
-      
+const Main = ({ allPokemonData }) => {
+
+    return (
+     <div><MDBTable>
+     <MDBTableHead>
+       <tr>
+         <th>#</th>
+         <th>First</th>
+         <th>Last</th>
+         <th>Handle</th>
+       </tr>
+     </MDBTableHead>
+     <MDBTableBody>
+       <tr>
+         <td>1</td>
+         <td>Mark</td>
+         <td>Otto</td>
+         <td>@mdo</td>
+       </tr>
+       <tr>
+         <td>2</td>
+         <td>Jacob</td>
+         <td>Thornton</td>
+         <td>@fat</td>
+       </tr>
+       <tr>
+         <td>3</td>
+         <td>Larry</td>
+         <td>the Bird</td>
+         <td>@twitter</td>
+       </tr>
+     </MDBTableBody>
+   </MDBTable></div>
+    );
+
+
+};
+
 export default Main;
